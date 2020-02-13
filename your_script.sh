2afe1966ba737a331fd52fa61480f43f8a4ffb02
Stephen Ribich <stephen.ribich@gmail.com>
2020-02-13 14:13:30 -0500

    release v1.1.0

--- your_script.sh
+++ your_script.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
-###############
-## Bash Starter
-###############
+#########################################################
+## Bash Starter (https://github.com/nulliel/bash-starter)
+#########################################################
 declare -g BOOTSTRAP_DIR && BOOTSTRAP_DIR="$(cd "${BASH_SOURCE[0]%/*}" && pwd)/lib"
+
+# shellcheck disable=SC2034
+declare -a ADDITIONAL_IMPORT_SOURCES=()
 
 # shellcheck source=lib/bootstrap
 source "${BOOTSTRAP_DIR}/bootstrap"
